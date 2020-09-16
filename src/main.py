26bd808828afa2bf9c0cdbd15699d3e0551559fd
Daniel Pavela <daniel.pavela@gmail.com>
2020-09-16 18:33:34 +1000

    Implement save functionality
    Signed-off-by: Daniel Pavela <daniel.pavela@gmail.com>

--- src/main.py
+++ src/main.py
@@ -23,7 +23,7 @@ from gi.repository import Gtk
 
 class Handler:
     def onSaveButtonPressed(self, button):
-        print("Hello World!")
+        save_as_file()
 
 
 builder = Gtk.Builder()
@@ -32,6 +32,60 @@ builder.connect_signals(Handler())
 
 window = builder.get_object("QuickencryptWindow")
 window.show_all()
+
+def save_as_file():
+    # Thanks to https://github.com/sriske2/umte for some of this code
+    """Prompt the user with a save dialog and write the file."""
+    save_dialog = Gtk.FileChooserDialog(
+        "Save As", window,
+        Gtk.FileChooserAction.SAVE,
+        (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+        Gtk.STOCK_SAVE, Gtk.ResponseType.OK))
+
+    # Make sure the user is warned if they're overwriting an existing file.
+    save_dialog.set_do_overwrite_confirmation(True)
+    # Suggest a generic filename.
+    save_dialog.set_current_name("untitled.txt")
+
+    response = save_dialog.run()
+    if response == Gtk.ResponseType.OK:
+        # Get the path and filename then save the file.
+        epicPath = save_dialog.get_filename()
+        # filename = os.path.basename(self.path)
+        write_file(epicPath)
+
+    elif response == Gtk.ResponseType.CANCEL:
+        pass
+
+    save_dialog.destroy()
+
+def write_file(file_path):
+        try:
+            _file = open(file_path, 'w', encoding='utf-8')
+        except IOError:
+            self.error("Unable to save" + file_path, "Check that you have proper permissions")
+            self.path = None
+            self.filename = None
+            return
+
+        # Get the buffer
+        epicBuffer = builder.get_object("textBuffer")
+
+        # Get the text from the buffer and add a \n to it
+        start, end = epicBuffer.get_bounds()
+        text = epicBuffer.get_text(start, end, False) + "\n"
+        # Write to the file and close it
+        _file.write(text)
+        _file.close()
+        #
+        epicBuffer.set_modified(False)
+
+        # Add the filename to the window's title
+        # Remove the modification status from the title since the file has been saved.
+
+        # TODO: Fix this
+        # self.title = self.filename + ' - ' + self.name
+        # self.set_title(self.title)
 
 Gtk.main()
 
